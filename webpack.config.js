6e3bea6a3ee99eb29cd2181accf7980c17fd51af
MFAC <ninafroes@gmail.com>
2020-09-06 19:10:23 +0200

    refactor: Delete comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
-
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
-/*
- * We've enabled MiniCssExtractPlugin for you. This allows your app to
- * use css modules that will be moved into a separate CSS file instead of inside
- * one of your module entries!
- *
- * https://github.com/webpack-contrib/mini-css-extract-plugin
- *
- */
-
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-/*
- * We've enabled TerserPlugin for you! This minifies your app
- * in order to load faster and run less javascript.
- *
- * https://github.com/webpack-contrib/terser-webpack-plugin
- *
- */
-
 const TerserPlugin = require('terser-webpack-plugin');
-
 const workboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
